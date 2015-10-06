4b1731acea5303b4dacf141f66a428bd2b7407c7
d59495 <andrea.manzi@comune.fi.it>
2015-10-06 15:33:17 +0200

    Progetto iniziale

--- DependencyInjection/OsFunctions.php
+++ DependencyInjection/OsFunctions.php
@@ -8,21 +8,26 @@ class OsFunctions {
      * La funzione restituisce dov'è installato php sulla macchina
      */
     static function getPHPExecutableFromPath() {
-        $phpPath = exec("which php");
-        if (file_exists($phpPath)) {
-            return $phpPath;
-        } elseif (file_exists("/usr/bin/php")) {
-            return "/usr/bin/php";
-        }
-        $paths = explode(PATH_SEPARATOR, getenv('PATH'));
-        foreach ($paths as $path) {
-            $php_executable = $path . DIRECTORY_SEPARATOR . "php" . (isset($_SERVER["WINDIR"]) ? ".exe" : "");
-            if (file_exists($php_executable) && is_file($php_executable)) {
-                return $php_executable;
+        if (self::isWindows()) {
+            //In caso di windows
+            $paths = explode(PATH_SEPARATOR, getenv('PATH'));
+            foreach ($paths as $path) {
+                $php_executable = $path . DIRECTORY_SEPARATOR . "php" . (isset($_SERVER["WINDIR"]) ? ".exe" : "");
+                if (file_exists($php_executable) && is_file($php_executable)) {
+                    return $php_executable;
+                }
+            }
+        } else {
+            //In caso altri sistemi operativi (linux)
+            $phpPath = exec("which php");
+            if (file_exists($phpPath)) {
+                return $phpPath;
+            } elseif (file_exists("/usr/bin/php")) {
+                return "/usr/bin/php";
             }
         }
-        echo "Php non trovato";
-        return FALSE; // not found
+        
+        throw new Exception("Php non trovato");
     }
 
     /**
@@ -37,13 +42,13 @@ class OsFunctions {
     }
 
 }
-    /**
-     * La funzione restituisce dov'è installato php sulla macchina
-     *
-     * @param $elem Oggetto da cercare
-     * @param $array Array nel quale cercare
-     * @param $key Nome della chiave nella quale cercare $elem
-     * @return Mixed False se non trovato l'elemento, altrimenti l'indice in cui si è trovato il valore
-     */
 
+/**
+ * La funzione restituisce eccc
+ *
+ * @param $elem Oggetto da cercare
+ * @param $array Array nel quale cercare
+ * @param $key Nome della chiave nella quale cercare $elem
+ * @return Mixed False se non trovato l'elemento, altrimenti l'indice in cui si è trovato il valore
+ */
 ?>
