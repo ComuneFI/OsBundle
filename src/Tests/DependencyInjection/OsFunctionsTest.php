75e2678eb544e088171741cef9fb41114644ff84
Andrea Manzi <manzolo@libero.it>
2016-09-23 22:05:13 +0200

    new test

--- src/Tests/DependencyInjection/OsFunctionsTest.php
+++ src/Tests/DependencyInjection/OsFunctionsTest.php
@@ -7,32 +7,31 @@ use Fi\OsBundle\DependencyInjection\OsFunctions;
 
 class OsFunctionsTest extends WebTestCase
 {
-    protected function setUp()
-    {
-        /*require_once __DIR__.'/../../AppKernel.php';
 
-        $kernel = new \AppKernel('test', true);
-        $kernel->boot();
-        $container = $kernel->getContainer();*/
-        
+    protected function setUp() 
+    {
+        /* require_once __DIR__.'/../../AppKernel.php';
+
+          $kernel = new \AppKernel('test', true);
+          $kernel->boot();
+          $container = $kernel->getContainer(); */
     }
 
-    public function testFunctions()
+    public function testFunctions() 
     {
         $this->assertContains('php', OsFunctions::getPHPExecutableFromPath());
         if (PHP_OS == 'WINNT') {
             $this->assertTrue(OsFunctions::isWindows());
             $this->assertEquals(OsFunctions::getSeparator(), "&");
-        }else{
-               $this->assertFalse(OsFunctions::isWindows());
-               $this->assertEquals(OsFunctions::getSeparator(), ";");
+        } else {
+            $this->assertFalse(OsFunctions::isWindows());
+            $this->assertEquals(OsFunctions::getSeparator(), ";");
         }
         $dirtest = sys_get_temp_dir() . DIRECTORY_SEPARATOR . "testosbundle";
         mkdir($dirtest);
         $this->assertTrue(file_exists($dirtest));
         OsFunctions::delTree($dirtest);
         $this->assertFalse(file_exists($dirtest));
-        
-        
     }
+
 }
